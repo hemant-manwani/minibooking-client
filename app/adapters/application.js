72f5f536f62e21f104e4db97f45910b2bb277661
Hemant <hemantdev.hts@gmail.com>
2017-10-12 19:13:59 +0530

    displayed validation errors

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -7,5 +7,14 @@ export default DS.RESTAdapter.extend({
     if (Ember.isEmpty(hash.data)) hash.data = {};
     hash.data.auth_token = 'f790216ba928874ebe19a240d54ce0'; // Add an access token param.
     return this._super(url, type, hash);
+  },
+  isInvalid(status, headers, payload){ 
+    $('.validation-error').html('');
+    var errors = []
+    if (status===422) { 
+      $.each( payload.errors, function( key, value ) {
+        $('.validation-error').append(Ember.String.classify(key) + ": " + value + '<br/>');
+      });
+    }
   }
 });
