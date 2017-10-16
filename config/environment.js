9e6c76e889d23a560173eedc0f767888dabf0a56
Hemant <hemantdev.hts@gmail.com>
2017-10-16 16:10:06 +0530

    Refactor config to ES6

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 
-module.exports = function(environment) {
-  var ENV = {
+module.exports = (environment) => {
+  let ENV = {
     modulePrefix: 'minibooking-client',
     environment: environment,
     rootURL: '/',
@@ -21,31 +21,32 @@ module.exports = function(environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     }
-  };
+  }
 
   if (environment === 'development') {
-    ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_RESOLVER = true
+    ENV.APP.LOG_ACTIVE_GENERATION = true
+    ENV.APP.LOG_TRANSITIONS = true
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true
+    ENV.APP.LOG_VIEW_LOOKUPS = true
     ENV.APP.API_ENDPOINT = "http://localhost:3000"
+    ENV.APP.AUTH_TOKEN = "f790216ba928874ebe19a240d54ce0"
   }
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.locationType = 'none';
+    ENV.locationType = 'none'
 
     // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
 
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.rootElement = '#ember-testing'
   }
 
   if (environment === 'production') {
     
   }
 
-  return ENV;
-};
+  return ENV
+}
