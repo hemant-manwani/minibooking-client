a6342b46353b2fb438b7b8709132701324aa9ed4
Hemant <hemantdev.hts@gmail.com>
2017-10-18 11:24:17 +0530

    added ENV variables

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,13 @@ module.exports = (environment) => {
   }
 
   if (environment === 'production') {
-    
+    ENV.APP.LOG_RESOLVER = true
+    ENV.APP.LOG_ACTIVE_GENERATION = true
+    ENV.APP.LOG_TRANSITIONS = true
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true
+    ENV.APP.LOG_VIEW_LOOKUPS = true
+    ENV.APP.API_ENDPOINT = process.env.API_URL
+    ENV.APP.AUTH_TOKEN = process.env.AUTH_TOKEN
   }
 
   return ENV
