57c303defae0db6cfcfcc65b2d893700aa7cc3c0
Hemant <hemantdev.hts@gmail.com>
2017-10-18 16:35:37 +0530

    quick fixed

--- app/controllers/rental.js
+++ app/controllers/rental.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ["page", "perPage"],
+  content: {},
 
   page: Ember.computed.alias("content.page"),
   perPage: Ember.computed.alias("content.perPage"),
